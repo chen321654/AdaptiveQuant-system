8eb09ed8911c8b8a245a8e887e1a0c22f51cb368
sakami <3209996299@qq.com>
2024-10-14 17:29:02 +0800

    初步股票模型

--- server/apps/DataShow/apps.py
+++ server/apps/DataShow/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DatashowConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "DataShow"
+    name = "apps.DataShow"
--- server/apps/DataShow/models.py
+++ server/apps/DataShow/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from utils.models import BaseModel
+
+class Stock(BaseModel):
+    id = models.CharField(max_length=64, primary_key=True, verbose_name="股票id")
+    open = models.FloatField(verbose_name="开盘价")
+    high = models.FloatField(verbose_name="最高价")
+    low = models.FloatField(verbose_name="最低价")
+    close = models.FloatField(verbose_name="收盘价")
+    volume = models.BigIntegerField(verbose_name="交易额")
+
+    class Meta:
+        db_table = "tb_order_info"
+        verbose_name = '股票信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.id
